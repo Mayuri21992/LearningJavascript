64f6d619dfdfb19b5f01e95f08286d32c1df95e6
Mayuri21992 <shendrem@gmail.com>
2024-05-31 13:25:40 +0000

    learned datatypes in js

---
+++ .devcontainer/01.Basics/datatype.js
@@ -0,0 +1,25 @@
+"use strict";//by writing this your code will treated as newer version
+//alert(3+3)//this is used in browser not in nodejs
+let name="mayuri"//string
+let age=31//number
+let height=5.1//
+let isLogged=true//boolean
+let greade='A'
+/*
+primitive datatype
+null is standalone datatype
+string 
+number
+boolean
+undefined
+symbol=uniqueness
+
+non primitive datatype
+object
+
+*/
+console.table(typeof name) //string
+console.log(typeof age)//number
+console.log(typeof height)//number
+console.log (typeof isLogged)//boolean
+console.log(typeof greade)//string
