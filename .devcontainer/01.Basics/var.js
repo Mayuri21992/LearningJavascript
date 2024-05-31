1e9f14a42294bb3b177c0a0712e8bd40fe2156f7
Mayuri21992 <shendrem@gmail.com>
2024-05-31 12:28:16 +0000

    variable creation in js is done

---
+++ .devcontainer/01.Basics/var.js
@@ -0,0 +1,6 @@
+const customerId=12345//const
+let customerName="mayuri"//global variable declaration
+var customerEmail="mayuri@mail.com"//scope declaration(not in use)
+customerCity="bengaluru"//bad practise of var declaration
+
+console.table([customerId,customerName,customerEmail,customerCity])
