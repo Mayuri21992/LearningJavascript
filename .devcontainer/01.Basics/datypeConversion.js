1fd109a4751752ea3d0ebb6806f95acceeea0ed9
Mayuri21992 <shendrem@gmail.com>
2024-05-31 14:00:20 +0000

    datatype conversin lerned in this file

---
+++ .devcontainer/01.Basics/datypeConversion.js
@@ -0,0 +1,11 @@
+//in this file we are going change the datatype
+let Id="33"//datatype is string conversion is possible
+//convert it into number
+let numberId= Number(Id)
+console.log(numberId,typeof numberId)
+
+let num="45abc"
+let numberNum= Number(num)
+console.log(numberNum,typeof numberNum)
+//it will print not an number number with letters not convertale it gives output as NaN
+
