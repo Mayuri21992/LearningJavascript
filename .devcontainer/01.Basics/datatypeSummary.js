83032e2917ed1495cbf02ad59bc84e3d34ac3a9d
Mayuri21992 <shendrem@gmail.com>
2024-06-04 10:20:57 +0000

    primitive/non-primitive datatype summery

---
+++ .devcontainer/01.Basics/datatypeSummary.js
@@ -0,0 +1,21 @@
+// primitive datatype(call by value)
+//7 types: string, number, boolean, symbol, undefined, null, bigInt
+let name="mayuri"
+let dayOfBirth=2
+let isNameisempty=false
+let year
+let temperature=null
+let bigNumber=1234567893245553
+
+
+
+//Non-primitive datatype(call by refernce)
+//array, objects, functions
+let colour=["red", "green", "pink"]
+let myboject={
+     num1:20,
+     Name:"mayurii"
+}
+const myfunction=function(){
+    console.log("helloworld");
+}
